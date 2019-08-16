3a20509546f011c590f5f1b2e004ef402df9ef89
Abdul Aldukhn <abbcc123123@gmail.com>
2019-08-17 01:54:50 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Abdul Aldukhn - Ruby on Rails - React - Web Developer - React Developer">
+  <meta name="author" content="Abdul Aldukhn">
+  <title>Abdul Aldukhn</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/brands.css"
+    integrity="sha384-Px1uYmw7+bCkOsNAiAV5nxGKJ0Ixn5nChyW8lCK1Li1ic9nbO5pC/iXaq27X5ENt"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/fontawesome.css"
+    integrity="sha384-BzCy2fixOYd0HObpx3GMefNqdbA7Qjcc91RgYeDjrHTIEXqiF00jKvgQG0+zY/7I"
+    crossorigin="anonymous">
+</head>
+
+<body class="bg-light">
+
+  <div class="container">
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4 rounded-circle" src="./abdul.jpeg" alt="Abdul Aldukhn" width="200" height="200">
+      <h1 class="h2">Abdul Aldukhn</h1>
+
+      <p class="mt-5">
+        My name is Abdulrahman, and I'm a Full Stack Developer based in Medina<br />
+       
+      </p>
+      <!-- <p>
+        I use Ruby on Rails and ReactJS on daily work,<br />
+        and I enjoy learning and trying other languages and frameworks.
+      </p>-->
+      <p>
+        If I'm not cooding, probably I'll be surfing GitHub or Youtube,<br />
+        learning new technologies and trying new tools and techniques.
+      </p>
+
+    </div>
+
+    <footer class="text-muted text-center text-small">
+      <p class="mb-3">You can find me at:</p>
+      <ul class="list-inline">
+        <li class="list-inline-item mx-3">
+          <a href="https://www.linkedin.com/in/abdulrahman-omar-aldukhn-2220b8118/" target="_blank" title="LinkedIn" class="text-black-50">
+            <i class="fab fa-linkedin fa-2x"></i>
+          </a>
+        </li>
+        <li class="list-inline-item mx-3">
+          <a href="https://github.com/aldukhn" target="_blank" title="GitHub" class="text-black-50">
+            <i class="fab fa-github fa-2x"></i>
+          </a>
+        </li>
+        <li class="list-inline-item mx-3">
+          <a href="https://twitter.com/AbdulrahmanHDH" target="_blank" title="Twitter" class="text-black-50">
+            <i class="fab fa-twitter fa-2x"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  </div>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122480457-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-122480457-2');
+  </script>
+</body>
+
+</html>
